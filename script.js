413774cccdd6073520ac1718a61c721aa91e45e4
samis <samilsully@gmail.com>
2020-09-02 16:04:09 -0700

    Added in consts and the functions to pull random characters

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Strings for each character type to pull from
@@ -15,23 +14,33 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numbers = "1234567890".split("");
 var special = "~!@#$%^&*()_+".split("");
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function getRandomLowerCase () {
-  return lowerCase
-}
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const lowerCaseEl = document.getElementById('lowercase');
+const upperCaseEl = document.getElementById('uppercase');
+const numberEl = document.getElementById('number');
+const specialEl = document.getElementById('special');
 
-function getRandomUpperCase () {
-  return upperCase
-}
 
-function getRandomLowerCase () {
-  return numbers
-}
+const randomFunc = {
+  lower: getRandomLowerCase,
+  upper: getRandomUpperCase,
+  number: getRandomNumber,
+  special: getRandomSpecial,
+};
 
-function getRandomLowerCase () {
-  return special
+function getRandomLowerCase() {
+  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+}
+function getRandomUpperCase() {
+  return upperCase[Math.floor(Math.random() * upperCase.length)];
+}
+function getRandomNumber() {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+function getRandomSpecial() {
+  return special[Math.floor(Math.random() * special.length)];
 }
